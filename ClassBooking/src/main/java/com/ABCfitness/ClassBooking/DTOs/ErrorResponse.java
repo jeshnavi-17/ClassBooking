17dc472d93677265c01c89bc80ad8b9d93179b7f
jeshnavi-17 <sreejeshnavi179@gmail.com>
2025-01-23 19:47:26 +0530

    Add files via upload

---
+++ ClassBooking/src/main/java/com/ABCfitness/ClassBooking/DTOs/ErrorResponse.java
@@ -0,0 +1,38 @@
+package com.ABCfitness.ClassBooking.DTOs;
+
+import java.util.Map;
+
+public class ErrorResponse {
+    private String error;
+    private String message;
+    private Map<String, String> details;
+
+    public ErrorResponse(String error, String message) {
+        this.error = error;
+        this.message = message;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Map<String, String> getDetails() {
+        return details;
+    }
+
+    public void setDetails(Map<String, String> details) {
+        this.details = details;
+    }
+}
